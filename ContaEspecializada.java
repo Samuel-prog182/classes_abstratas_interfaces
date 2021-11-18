b05dc3b9501394ae3520423a02dcb8824f9697ff
Samuel-prog182 <samuellb.o.w.182@hotmail.com>
2021-11-18 19:47:37 +0000

    Conta

---
+++ ContaEspecializada.java
@@ -0,0 +1,12 @@
+public class ContaEspecializada extends Conta {
+
+    @Override
+    public void imprimeExtrato(){
+        System.out.println("Seu saldo é:" getSaldo);
+
+    }
+
+
+    
+    
+}
---
+++ Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        ContaEspecializada contaEspecializada=new ContaEspecializada();
+        //Conta
+        contaEspecializada.getSaldo(25000);
+        //ContaEspecializada
+        contaEspecializada.imprimeExtrato();
+        
+    }
+    
+}
